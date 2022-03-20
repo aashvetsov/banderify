47f6ef968e4dc5088a4f385f5fb53655b9667304
Artem Shvetsov <artem.shvetsov@raiffeisen.ua>
2022-03-20 23:32:33 +0200

    feat: repositories set properly forming

--- Sources/bearhunterlib/Scanning/Repository.swift
+++ Sources/bearhunterlib/Scanning/Repository.swift
@@ -1,7 +1,26 @@
-public struct Repository: Hashable {
+public struct Repository {
 
     let name: String
     let url: String
 }
 
+extension Repository: Hashable {
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(url.gitSuffixIgnoring)                        
+    }
+    
+    public static func == (lhs: Repository, rhs: Repository) -> Bool {
+        lhs.url.gitSuffixIgnoring == rhs.url.gitSuffixIgnoring
+    }
+}
+
 public typealias Repositories = Set<Repository>
+
+fileprivate extension String {
+
+    var gitSuffixIgnoring: String {
+        let gitSuffix = ".git"
+        return hasSuffix(gitSuffix) ? replacingOccurrences(of: gitSuffix, with: "") : self
+    }
+}
